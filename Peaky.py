9290c9f64aeb6f9f0f89a6f23693c00fc28a648f
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-09-09 09:37:01 +0530

    Update Peaky.py

--- Peaky.py
+++ Peaky.py
@@ -2,3 +2,25 @@
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from bot import Translation
+
+
+@trojanz.on_message(filters.command('start') & filters.private)
+async def start(client, message):
+    await message.reply_text(
+        text=Script.START_MSG.format(message.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("♻️ 𝙂𝙍𝙊𝙐𝙋", url="https://t.me/cinemazilla"),
+                    InlineKeyboardButton("👨‍💻 𝙈𝘼𝙎𝙏𝙀𝙍", url="https://t.me/no_ones_like_me")
+                ],
+                [
+                    InlineKeyboardButton("💿 𝙅𝙊𝙄𝙉 𝙊𝙐𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇 💿", url="https://t.me/joinchat/CXRICR1ok3ViZjk9")
+                ],
+                [
+                    InlineKeyboardButton("🔐 𝘾𝙇𝙊𝙎𝙀", callback_data="close_data"),
+                    InlineKeyboardButton("💡𝙃𝙀𝙇𝙋", callback_data="help_data"),
+                ]
+            ]
+        ),
